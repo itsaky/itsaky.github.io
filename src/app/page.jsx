9a40954ba3903dc90bc7ee255d26eb3681ed3aaa
Akash Yadav <itsaky01@gmail.com>
2024-02-16 22:42:53 +0530

    fix: set target='_blank' for links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,7 +32,8 @@ const HomePage = () => {
             </div>
 
             <Link href={GitHub_AndroidIDE}
-                  className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mt-12">
+                  className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mt-12"
+                  target="_blank">
                 <div className="flex items-center">
                     <button type="button">
                         I'm currently working on AndroidIDE
@@ -41,7 +42,8 @@ const HomePage = () => {
                 </div>
             </Link>
             <Link href={`${Dev.Social.GitHub.url}?tab=repositories`}
-                  className="mt-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                  className="mt-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  target="_blank">
                 <div className="flex items-center">
                     <button type="button">
                         View projects on GitHub
